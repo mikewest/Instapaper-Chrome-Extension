1c05097d226bdd5eceeaf448cde39fa6c7118959
Mike West <mike@mikewest.org>
2010-07-05 19:31:07 +0200

    Adding alt-p alias for windows users.

--- injected.js
+++ injected.js
@@ -1,6 +1,7 @@
 // keyboard shortcut
 window.addEventListener('keydown', function(e) {
-    if ( e.ctrlKey && e.keyCode === 80 ) { // Hard-coding `ctrl-p`.  Thanks NetNewsWire.
+    if ( ( e.ctrlKey || e.altKey ) && e.keyCode === 80 ) {
+    // Hard-coding `ctrl-p` and `alt-p`.  Thanks NetNewsWire.
         chrome.extension.sendRequest(
             { "action":   "sendToInstapaper" },
             function () {}
